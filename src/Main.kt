c69f5d39bb5f753ce42b417beaef680b64a2f0b1
ZotovaIrina <zotovaiv87@gmail.com>
2018-04-01 17:12:55 -0400

    Add button and listener

--- src/Main.kt
+++ src/Main.kt
@@ -4,9 +4,14 @@ fun main(args: Array<String>) {
     val message = "Hello World!!!";
     val template = """
         <h1 class="header">$message</h1>
+        <button id="button">Click</button>
         """
     println(message);
     val root = document.getElementById("root");
     // !! means if root is null don't do this
     root!!.innerHTML = template;
+    val buttonClick = document.getElementById("button");
+    buttonClick!!.addEventListener("click", {
+        println("Clicked");
+    })
 }
