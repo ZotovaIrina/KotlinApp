34af39704bfa632f83c7adba70f4185c7678dbf1
ZotovaIrina <zotovaiv87@gmail.com>
2018-04-03 11:28:09 -0400

    Creat Sum function

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,24 @@
 import kotlin.browser.document
 
+import sum
+
 fun main(args: Array<String>) {
-    val message = "Hello World!!!";
+    val message = "Sum calculation ";
     val root = document.getElementById("root");
     val div = document.createElement("div");
     val button = document.createElement("button");
     val h1 = document.createElement("h1");
+    val span = document.createElement("span");
 
     h1.textContent = message
-    button.textContent = "Click";
+    button.textContent = "Sum";
     button.addEventListener("click", {
-        println("Click");
+        span.textContent = "Sum is " + sum(1, 2)
     })
 
     div.appendChild(h1);
     div.appendChild(button);
+    div.appendChild(span);
     // !! means if root is null don't do this
     root!!.appendChild(div);
 }
